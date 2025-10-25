182d040adb9c3379f3a12bcb9efd4e8b57a5eeb3
avn-dev <vahap.caliskan@proton.me>
2025-10-25 20:11:30 +0200

    api changes

--- app/Http/Resources/AllergenResource.php
+++ app/Http/Resources/AllergenResource.php
@@ -17,6 +17,7 @@ class AllergenResource extends JsonResource
         return [
             'id'   => $this->id,
             'name' => $this->name,
+            'emoji' => $this->emoji,
         ];
     }
 }
