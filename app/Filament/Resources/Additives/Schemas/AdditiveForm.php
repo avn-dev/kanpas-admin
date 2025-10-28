72f87c2a73563780355f30ce9050bb583c2f8f48
avn-dev <vahap.caliskan@proton.me>
2025-10-28 23:01:45 +0100

    varipsu changes

--- app/Filament/Resources/Additives/Schemas/AdditiveForm.php
+++ app/Filament/Resources/Additives/Schemas/AdditiveForm.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Resources\Additives\Schemas;
 
-namespace App\Filament\Resources\Allergens\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
