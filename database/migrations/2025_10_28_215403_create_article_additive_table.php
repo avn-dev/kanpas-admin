0f8dac8f2f1cb04e64cf51c00cd33b68226f476c
avn-dev <vahap.caliskan@proton.me>
2025-10-28 23:03:52 +0100

    varipsu changes

--- database/migrations/2025_10_28_215403_create_article_additive_table.php
+++ database/migrations/2025_10_28_215403_create_article_additive_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('article_additive', function (Blueprint $table) {
+        Schema::create('additive_article', function (Blueprint $table) {
             $table->id();
             $table->foreignId('additive_id')
                 ->references('id')
