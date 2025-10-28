b96939a8491df8997226103ea048c604e7815135
avn-dev <vahap.caliskan@proton.me>
2025-10-28 22:41:11 +0100

    various changes

--- app/Http/Resources/AllergenResource.php
+++ app/Http/Resources/AllergenResource.php
@@ -14,10 +14,6 @@ class AllergenResource extends JsonResource
      */
     public function toArray($request): array
     {
-        return [
-            'id'   => $this->id,
-            'name' => $this->name,
-            'emoji' => $this->emoji,
-        ];
+        return parent::toArray($request);
     }
 }
