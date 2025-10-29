77ee3f2cff34e0d33cdd8fb5d6b2595af77ce897
avn-dev <vahap.caliskan@proton.me>
2025-10-29 18:50:33 +0100

    number fix

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -20,12 +20,14 @@ class ArticleResource extends JsonResource
             'name'            => $this->name,
             'description'     => $this->description,
             'image_path'      => $this->image_path,
+            'number'          => $this->number,
             'price'           => $this->price, // entweder gesetzt ODER null (bei OptionGroup)
             'effective_price' => $this->whenAppended('effectivePrice', $this->effectivePrice()),
             'option_group'    => new OptionGroupResource($this->whenLoaded('optionGroup')),
             'allergens'       => AllergenResource::collection($this->whenLoaded('allergens')),
             // Optionen inkl. Pivot-Preis:
             'options'         => ArticleoptionResource::collection($this->whenLoaded('options')),
+            'additives'       => AdditiveResource::collection($this->whenLoaded('additives')),
         ];
     }
 }
